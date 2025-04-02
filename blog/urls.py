a77fef563ce0745e83be34dbe49288284dcaca6a
ViktorKryzhnii <vkryznij6@gmail.com>
2025-04-02 15:23:01 +0300

    Publish, login, logout

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     path('^post/new/$', views.post_new, name='post_new'),
     path('^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
 ]
